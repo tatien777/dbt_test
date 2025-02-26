0c6afe645fc7c72d4a4eb0247ed6f86c22bb7fa5
tatien777 <tientm0409@gmail.com>
2025-02-27 00:14:10 +1030

    added index doc

--- datawarehouse/models/dimensions/dim_locations.sql
+++ datawarehouse/models/dimensions/dim_locations.sql
@@ -1,12 +1,12 @@
-{{
-    config(
-        materialized='table',
-        alias='stationlocations'
-    )
-}}
+    {{
+        config(
+            materialized='table',
+            alias='stationlocations'
+        )
+    }}
 
-select
-    cast(locationid as varchar(6)) as location_id,
-    cast(locationname as varchar(50)) as location_name
-from test 
-where locationid is not null
+    select
+        cast(locationid as varchar(6)) as location_id,
+        cast(locationname as varchar(50)) as location_name
+    from {{ ref('stationLocations') }}
+    where locationid is not null
--- datawarehouse/models/staging/stg_daily_rainfall.sql
+++ datawarehouse/models/staging/stg_daily_rainfall.sql
@@ -3,4 +3,11 @@
     config(materialized='table')
 }}
 
-select * from datawarehouse_staging
+select
+    location_id,
+    date_trunc('day', observation_time) as date,
+    sum(rainfall_mm) as total_daily_rainfall,
+    avg(rainfall_mm) as avg_hourly_rainfall,
+    max(rainfall_mm) as peak_hourly_rainfall
+from test
+group by 1, 2
