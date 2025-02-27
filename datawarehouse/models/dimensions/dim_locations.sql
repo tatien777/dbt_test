2d7b0d833a4b90730bfa2bf86f29d9068b9d425e
tatien777 <tientm0409@gmail.com>
2025-02-28 02:01:47 +1030

    fix the source

--- datawarehouse/models/dimensions/dim_locations.sql
+++ datawarehouse/models/dimensions/dim_locations.sql
@@ -1,7 +1,12 @@
-{{ config(materialized='table') }}
+{{ 
+    config(
+        materialized='table',
+        schema='weather'
+    ) 
+}}
 
-    select
-        cast(locationid as varchar(6)) as location_id,
-        cast(locationame as varchar(50)) as location_name
-    from {{ ref('station_locations') }}
-    where locationid is not null
+select
+    cast(locationid as varchar(6)) as location_id,
+    cast(locationame as varchar(50)) as location_name
+from {{ source('weather', 'raw_station_locations') }}  
+where locationid is not null
