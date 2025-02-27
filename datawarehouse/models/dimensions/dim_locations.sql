21a2c1ddf6a65d62a63a5ecf55b33bebc2b826db
tatien777 <tientm0409@gmail.com>
2025-02-28 00:21:43 +1030

    rename the station locations

--- datawarehouse/models/dimensions/dim_locations.sql
+++ datawarehouse/models/dimensions/dim_locations.sql
@@ -1,12 +1,7 @@
-    {{
-        config(
-            materialized='table',
-            alias='stationlocations'
-        )
-    }}
+{{ config(materialized='table') }}
 
     select
         cast(locationid as varchar(6)) as location_id,
-        cast(locationname as varchar(50)) as location_name
-    from {{ ref('stationLocations') }}
+        cast(locationame as varchar(50)) as location_name
+    from {{ ref('station_locations') }}
     where locationid is not null
